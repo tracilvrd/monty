25ba92c25fdf84811b469204d3d4bc667fdce5ee
tracilvrd <oghenekparobo.ese@gmail.com>
2022-12-16 05:49:54 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -76,6 +76,7 @@ void op_pall(stack_t **stack, unsigned int line_number);
 void op_pint(stack_t **stack, unsigned int line_number);
 void op_pop(stack_t **stack, unsigned int line_number);
 void op_swap(stack_t **stack, unsigned int line_number);
+size_t getline(char **lineptr, size_t *n, FILE *stream);
 
 char **split_line(char *line);
 void (*get_op_func(char *s))(stack_t**, unsigned int);
